49476224fb6c7339fb3c6526c8d094fd0d5296cd
PoppyRed91 <pfercek@gmail.com>
2023-12-21 19:34:42 +0100

    finished tutorial

---
+++ second-assignment-react/src/App.tsx
@@ -0,0 +1,51 @@
+//import ListGroup from "./components/ListGroup";
+
+//import Alert from "./components/Alert";
+
+import Button from "./components/Button"
+
+import { useState } from "react";
+
+import Alert from "./components/Alert";
+
+function App() {
+    /* 
+    let items = [
+        "New York",
+        "San Francisco",
+        "Tokyo",
+        "London",
+        "Paris"
+    ];
+
+    const handleSelectItem = (item: string) => {
+        console.log(item);
+    }
+
+    return (
+        <div>
+            <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem} />
+        </div>
+    )
+    
+
+    return (
+        <div>
+            <Alert>
+                Hello <span>World</span>
+            </Alert>
+        </div>
+    );
+    */
+
+    const [alertVisible, setAlertVisibility] = useState(false);
+
+    return (
+        <div>
+            { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
+            <Button color="primary" onClick={() => setAlertVisibility(true)}>Button</Button>
+        </div>
+    )
+}
+
+export default App;
---
+++ second-assignment-react/src/Message.tsx
@@ -0,0 +1,12 @@
+// create react component
+
+function Message() {
+    const name = "Petra";
+
+    if (name) 
+        return <h1>Hello {name}</h1>;
+    
+    return <h1>Hello World</h1>;
+}
+
+export default Message
